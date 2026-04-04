b96619fcc949a1e86ed5c22bafcfe09e5bc184f2
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2026-04-04 17:35:16 +0530

    Update take_test.py

--- modules/take_test.py
+++ modules/take_test.py
@@ -9,10 +9,10 @@ from database.db_manager import insert_row
 from config.settings import TEST_SESSIONS_FILE, QUESTION_LOGS_FILE
 
 from rag.rag_pipeline import load_rag_pipeline
-#from agents.question_agent import generate_questions
-#from agents.hint_agent import generate_hint
-#from agents.explain_agent import generate_explanation
-#from agents.example_agent import generate_example
+from .question_agent import generate_questions
+from .hint_agent import generate_hint
+from .explain_agent import generate_explanation
+from .example_agent import generate_example
 
 from agents.question_agent import generate_questions
 from agents.hint_agent import generate_hint
